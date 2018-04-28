5a502aad4412cebab8036e8447e440eb7442baff
Duncan <loxa10077@gmail.com>
2018-04-28 14:44:40 +0800

    add uplimage oder using carrierwave gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
   validates_presence_of :name, :body
+
+  def count
+    "寫能算出comment的方法"
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   validates_presence_of :title, :body
+
+  mount_uploader :image, ImageUploader
 end
